3aebec973dcc2e972c90fabdf674962aa01f9956
Joshua M <joshua.moon10@gmail.com>
2022-01-10 22:35:45 -0500

    This commit contains the finished PyPoll folder and it's main.py file, as well as the updated readme for the repository

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,66 +8,55 @@ datapath = os.path.join('Resources','election_data.csv')
 #open and read csv
 with open(datapath, mode='r') as df:
     reader = csv.reader(df)
-    
-    #skip header
-    next(df)
+
+    next(df)            #skip header
 
     #create variables
-    totalvotes = 0                  #create variable for counting votes
-    c1votes = 0
-    c2votes = 0
-    c3votes = 0
-    c4votes = 0
-    prev_val = None                 #previous value in array
+    totalvotes = 0      #for counting votes
+    c0votes = 0         #candidate 1's total votes
+    c0percent = 0       #candidate 1's percent of total votes
+    c1votes = 0         #candidate 2's total votes
+    c1percent = 0       #candidate 2's percent of total votes
+    c2votes = 0         #candidate 3's total votes
+    c2percent = 0       #candidate 3's percent of total votes
+    c3votes = 0         #candidate 4's total votes
+    c3percent = 0       #candidate 4's percent of total votes
 
     #create lists       
-    candidates = []                 #for csv column "candidates"
-    uniquecandidates = []           #for list of each unique candidate
-
+    candidate = []      #list of each unique candidate
+  
     #loop through data
     for row in reader:
-        totalvotes += 1                 #calc total number of votes
-        candidates.append(row[2])       #fill list candidates (per vote)
+        totalvotes += 1                #calc total number of votes
+        if row[2] not in candidate:    #check if candidate name has been added to list
+            candidate.append(row[2])   #fill list candidates (per vote)     
+        if row[2] == candidate[0]:     #check if cand. name is candidate 1
+            c0votes += 1               #tally candidate 1's votes
+        elif row[2] == candidate[1]:   #candidate 2?
+            c1votes += 1                #tally candidate 2's votes
+        elif row[2] == candidate[2]:   #candidate 3?
+            c2votes += 1                #tally candidate 3's votes
+        elif row[2] == candidate[3]:   #candidate 4?
+            c3votes += 1                #tally candidate 4's votes
 
-        #loop through candidate column list
-        for val in candidates:
-            if val not in uniquecandidates:     #if candidate name is not in uniquecandidates list
-                uniquecandidates.append(val)    #add the name to the list
-    
-    if row[2] == uniquecandidates[0]:
-       c1votes += 1
-    #if row[2] == uniquecandidates[1]:
-    #   c2votes += 1
-    #if row[2] == uniquecandidates[2]:
-    #   c3votes += 1
-    #if row[2] == uniquecandidates[3]:
-    #   c4votes += 1
-    print(c1votes)
-#print(uniquecandidates[0])
-#print(c1votes)
+#calculate each candidate's percent votes of total
+c0percent = "{:.00%}".format(c0votes / totalvotes)      #candidate 1
+c1percent = "{:.00%}".format(c1votes / totalvotes)      #candidate 2
+c2percent = "{:.00%}".format(c2votes / totalvotes)      #candidate 3
+c3percent = "{:.00%}".format(c3votes / totalvotes)      #candidate 4
 
-    #votetotals = {uniquecandidates[0]:c1votes, uniquecandidates[0]:c2votes}
-    #print(votetotals)   
-    
-    #create a dictionary called castvotes that contains a
-    #castvotes = {row[0]:row[1]}     #fill dictionary with voteid and candidate name
-        
-#with open(datapath) as datafile:
-#    reader = csv.reader(datafile, delimiter=",")
-
-    #skip header
-#    next(datafile)
-
-    #create variables 
-#    votecount=0         #to count votes
-    
-    #for loop to read csv
-#    for rows in csv.reader(df):
-#        if rows[2] == uniquecandidates[0]:
-#            c1votes += 1
-#    print(c1votes)
-
-
+#find who had the most votes
+maxvotes = max(c0votes, c1votes, c2votes, c3votes)
+if maxvotes == c0votes:
+    maxcand = candidate[0]
+elif maxvotes == c1votes:
+    maxcand = candidate[1]
+elif maxvotes == c2votes:
+    maxcand = candidate[2]
+elif maxvotes == c3votes:
+    maxcand = candidate[3]
+elif maxvotes == c4votes:
+    maxcand = candidate[4]
 
 #Print information to terminal and export text file with results
 print(f"""
@@ -76,30 +65,30 @@ Election Results
 -------------------------
 Total Votes: {totalvotes}
 -------------------------
-: ()
-Correy: ()
-Li: ()
-O'Tooley: ()
+{candidate[0]}: {c0percent} ({c0votes})
+{candidate[1]}: {c1percent} ({c1votes})
+{candidate[2]}: {c2percent} ({c2votes})
+{candidate[3]}: {c3percent} ({c3votes})
 -------------------------
-Winner:
+Winner: {maxcand}
 -------------------------
 ```
 """)
 
 #save file to .bas in analysis folder
-#with open('C:/Users/Joshua/Desktop/GT Bootcamp/gt-virt-data-pt-12-2021-u-c-master/03-Python/Homework/python--challenge/PyPoll/analysis/PyPollAnalysis.bas', 'w') as f:
-#    f.write(f"""
-#```
-#Election Results
-#-------------------------
-#Total Votes: {totalvotes}
-#-------------------------
-#Khan: ()
-#Correy: ()
-#Li: ()
-#O'Tooley: ()
-#-------------------------
-#Winner:
-#-------------------------
-#```
-#""")
+with open('C:/Users/Joshua/Desktop/GT Bootcamp/gt-virt-data-pt-12-2021-u-c-master/03-Python/Homework/python--challenge/PyPoll/analysis/PyPollAnalysis.bas', 'w') as f:
+    f.write(f"""
+```
+Election Results
+-------------------------
+Total Votes: {totalvotes}
+-------------------------
+{candidate[0]}: {c0percent} ({c0votes})
+{candidate[1]}: {c1percent} ({c1votes})
+{candidate[2]}: {c2percent} ({c2votes})
+{candidate[3]}: {c3percent} ({c3votes})
+-------------------------
+Winner:
+-------------------------
+```
+""")
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # python--challenge
 This is the repository for HW 3 - "Py Me Up, Charlie"
+
+With in this repository there are two main folders - PyBank and PyPoll.
+
+In the PyBank folder there is a main.py file that will iterate through the budget_data.csv file in the Resources folder. The code will read through the csv data and extract some financial analysis:
+    The total number of months represented 
+        (by counting all rows)
+    The total amount of profits and losses summed together 
+        (summing all values in the P/L row)
+    The average change from month to month 
+        (by finding the difference between the previous row and the current row, appending each difference value to a list)
+    And the greatest increase and greatest decrease in profits by month 
+        (found by taking the max and min of the monthly changes and finding their corresponding months)
+    The code then prints this analysis to the terminal and to a .bas file to the "analysis" folder.
+
+In the PyPoll folder there are all the same elements, except for the data file (election_data.csv) in the Resources folder. The code will read through the csv data and extract some financial analysis:
+    The total number of votes cast 
+        (by counting all rows)
+    The name of each individual candidate 
+        (by adding each unique value to a list)
+    The total number of votes cast  for each candidate 
+        (by checking if "candidate" value is candidate 1, adding one for each time they come up)
+    The percentage of the total number of votes that each candidate received
+        (their number of votes divided by the total)
+    The candidate with the most votes 
+        (by checking if the maximum number of votes equal each candidate's number of votes)
